a606e4970365ae39218ac4acf20b0be31391f5a1
cokaroach <roche.liam.email@gmail.com>
2020-05-10 18:00:55 +1000

    more context

--- Idiot1.py
+++ Idiot1.py
@@ -3,7 +3,18 @@ from AiC4 import AiC4
 class Idiot(AiC4):
     def __init__(self):
         pass
-    
+
+"""
+       board is a 2d array of the connect 4 board
+        board[0] is the first column
+        board[0][0] is the bottom cell in the first column
+        values will be one of:
+         - True = your token
+         - False = oponents token
+         - None = empty
+        
+        return value should be the index column where you want to insert your next token
+"""    
     def takeTurn(self, board):
         for x in range(7):
             if(board[x][5] is None):
