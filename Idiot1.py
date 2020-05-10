626a2e0ba64ad0254071d241b9b69e1f906c96d1
cokaroach <roche.liam.email@gmail.com>
2020-05-10 18:04:05 +1000

    fix indenting

--- Idiot1.py
+++ Idiot1.py
@@ -4,7 +4,7 @@ class Idiot(AiC4):
     def __init__(self):
         pass
 
-"""
+    """
        board is a 2d array of the connect 4 board
         board[0] is the first column
         board[0][0] is the bottom cell in the first column
@@ -14,7 +14,7 @@ class Idiot(AiC4):
          - None = empty
         
         return value should be the index column where you want to insert your next token
-"""    
+    """    
     def takeTurn(self, board):
         for x in range(7):
             if(board[x][5] is None):
